04be822fd39d78f76fb3b744adaaa170da0d4055
Cheese <clbisk@gmail.com>
2016-11-13 10:38:11 -0600

    No cursor plea

--- typed.js
+++ typed.js
@@ -417,7 +417,7 @@
 		// false = infinite
 		loopCount: false,
 		// show cursor
-		showCursor: true,
+		showCursor: false,
 		// character for cursor
 		cursorChar: "|",
 		// attribute to type (null == text)
