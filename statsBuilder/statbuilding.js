328df958bc79e832dfc9bdf726a43ec2a081c7e5
Cheese <clbisk@gmail.com>
2017-04-06 00:30:19 -0500

    Started new project
    oops so many things to do

---
+++ statsBuilder/statbuilding.js
@@ -0,0 +1,6 @@
+//function to empty, then fill all the selects
+//in class '.classes' with the same values
+
+//function to gray out a class that's already
+//been selected
+	//$('selected thing').attr('disabled', 'disabled')
