b85a5b668b7fc78d369e9971acc6253823d7a1e6
Cheese <clbisk@gmail.com>
2017-04-06 20:56:40 -0500

    Started a li'l

--- statsBuilder/statbuilding.js
+++ statsBuilder/statbuilding.js
@@ -1,6 +1,31 @@
-//function to empty, then fill all the selects
-//in class '.classes' with the same values
+//function to create seven selects in '#senior' with the
+//course options
+function seniorClasses() {
+	var sel1 = createSelect('#screen', 'first', "First Period:");
+	var classVals = ['math', 'art'];
+	var classText = ["Pre-calc", "Sketching"];
+	sel1.fillSelect(classVals, classText);
+}
 
-//function to gray out a class that's already
+//helper function to create a label and select
+//with an id and label text inside specified object
+function createSelect(place, id, label) {
+	$(place).append(`
+			<label for=` + id + `>` + label + `</label>
+			<select id=` + id + `></select>
+	`);
+	return $(place).$('label');
+}
+
+//helper function to fill a select with some values and text
+function fillSelect(value, text) {
+	for (i = 0; i < value.length; i++) {
+		this.append(`
+				<option value=` + value[i] + `>` + text[i] + `</option>
+		`);
+	}
+}
+
+//function to gray out a selection that's already
 //been selected
 	//$('selected thing').attr('disabled', 'disabled')
