4c56f4b57ba5097285f75d2511374425fa5ea8b2
Cheese <clbisk@gmail.com>
2017-04-09 20:44:52 -0500

    Fixed formatting

--- statsBuilder/statbuilding.js
+++ statsBuilder/statbuilding.js
@@ -1,14 +1,19 @@
 //function to create seven selects in '#senior' with the
 //course options
 function seniorClasses() {
+	//text and values for the selects
 	var labelVals = ['1', '2', '3', '4', '5', '6', '7'];
 	var labelText = ["First Period", "Second Period", "Third Period", "Fourth Period", "Fifth Period", "Sixth Period", "Seventh Period"];
+	
+	//text and values for the options
+	var classVals = ['default', 'math', 'art', 'science', 'lit', 'cooking', 'history', 'music'];
+	var classText = ["Please Choose", "Pre-calc", "Sketching", "Chemistry", "Modern Literature", "Culinary Arts", "Acient Civilizations", "Chorus"];
+	
+	//use up all the texts to make all the selects and options
 	for (var i = 0; i < labelVals.length; i++) {
-		$('body').append("iteration " + i);
-		var classVals = ['default', 'math', 'art', 'science', 'lit', 'cooking', 'history', 'music'];
-		var classText = ["Please Choose", "Pre-calc", "Sketching", "Chemistry", "Modern Literature", "Culinary Arts", "Acient Civilizations", "Chorus"];
-		fillSelect(sel, classVals, classText);
+		var sel = createSelect('#screen', labelVars[i], labelText[i]);
 	}
+	fillSelects(sel, classVals, classText);
 }
 
 //helper function to create a label and select
@@ -19,17 +24,15 @@ function createSelect(place, id, label) {
 			<select id=` + id + `></select>
 			<br>
 	`);
-	return $(place + ' select');
 }
 
-//helper function to fill a select with some values and text
+//helper function to fill all selects at a location with some values and text
 function fillSelect(place, value, text) {
 	for (var i = 0; i < value.length; i++) {
-		place.append(`
+		$(place + ' select').append(`
 				<option value=` + value[i] + `>` + text[i] + `</option>
 		`);
 	}
-	return $(place);
 }
 
 //function to gray out a selection that's already
