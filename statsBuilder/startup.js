74803200eed4cc13dae69e542682f5fc2aa7ed0e
Cheese <clbisk@gmail.com>
2017-04-10 18:52:50 -0500

    plea why

--- statsBuilder/startup.js
+++ statsBuilder/startup.js
@@ -1,5 +1,8 @@
 //at startup
 $(document).ready(function() {
+	//bind event listener to created selectors
+	$(document).on('change', 'select', menuchange);
+	
 	//start first scene
 	seniorSelect();
 });
