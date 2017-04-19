ce3f5dea7c482d82fa8bd52470878ed25661c09a
Cheese <clbisk@gmail.com>
2017-04-18 19:07:10 -0500

    Temp fix for form

--- statsBuilder/startup.js
+++ statsBuilder/startup.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	//bind event listener to selectors and buttons
 	$('#screen').on('selectmenuchange', '.selector', menuchange);
 	$('#screen').on('click', '.reset', reset);
-	$('#screen').on('submit', '#seniorform', seniorformsubmit);
+	//$('#screen').on('submit', '#seniorform', seniorformsubmit);
 	
 	//start first scene
 	seniorSelect();
