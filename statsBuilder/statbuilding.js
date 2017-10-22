4e544d0f04e46607bff8dcf659acdaa54a5de721
Cheese <clbisk@gmail.com>
2017-10-21 20:45:03 -0400

    Added basics to baby stage

--- statsBuilder/statbuilding.js
+++ statsBuilder/statbuilding.js
@@ -1,19 +1,62 @@
-/**Prototype for when you such a li'l baby child**/
+/*eslint-env jquery */
+/**
+ * @name Baby
+ * @description Prototype for when you such a li'l baby child
+ * @class
+ */
 function Baby() {
 	this.language;
 	this.imagination;
 	this.coordination;
 }
 
+/**
+ * @name Baby.prototype.birth
+ * @description description
+ * @function
+ * @returns returns
+ */
 Baby.prototype.birth = function () {
 	this.language = (Math.random() * 2) + 1;
 	this.imagination = (Math.random() * 2) + 1;
 	this.coordination = (Math.random() * 2) + 1;
 	
 	$("#sidebar").append(`
+		<div id='stats'>
 			<div id=language>Language: ` + Math.trunc(this.language) + `</div>
 			<div id=imagination>Imagination: ` + Math.trunc(this.imagination) + `</div>
 			<div id=coordination>Coordination: ` + Math.trunc(this.coordination) + `</div>
+		</div>
+		<div id='needs'>
+			<div class='progressbar'>hunger</div>
+			<div class='progressbar'>energy</div>
+			<div class='progressbar'>hygene</div>
+			<div class='progressbar'>social</div>
+		</div>
 	`);
 	
+	$("#needs div").progressbar({
+		max: 20,
+		value: 20
+	});
+	
+	$("#screen").append(`
+		<div id="actions">
+			<button class='language'>make random noises</button>
+			<button class='coordination'>make fists</button>
+			<button>sleep</button>
+		</div>
+	`);
+	
+	$("#actions button").on("click", doAction);
+};
+
+function doAction( event ) {
+	var statUp = $(this).attr(class);
+}
+
+function firstSkill() {
+	$(document).append(`
+		<div id="skills"></div>
+	`);
 }
