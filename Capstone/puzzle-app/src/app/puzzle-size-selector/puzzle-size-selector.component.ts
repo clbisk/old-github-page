4f8a7c20d614ae0e1342ff6cc69002705c8296e9
Cecilia Bisk <clbisk@gmail.com>
2020-01-24 11:52:42 -0500

    added ngbdropdown

--- Capstone/puzzle-app/src/app/puzzle-size-selector/puzzle-size-selector.component.ts
+++ Capstone/puzzle-app/src/app/puzzle-size-selector/puzzle-size-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-puzzle-size-selector',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuzzleSizeSelectorComponent implements OnInit {
 
+  @Input() puzzleType;
   constructor() { }
 
   ngOnInit() {
+    console.log(puzzleSizes[this.puzzleType]);
   }
 
 }
+
+export const puzzleSizes = {"Hitori": ["5x5", "6x6", "7x7", "8x8", "9x9"], "Heyawake": ["9x9", "10x10", "11x11", "15x15"]};
