258a5ee933ea015810cf2ee5e9b9d9d38e282842
Cecilia Bisk <clbisk@gmail.com>
2020-01-24 17:37:09 -0500

    added navset to landing page

--- Capstone/puzzle-app/src/app/app.module.ts
+++ Capstone/puzzle-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { AppComponent } from './app.component';
 import { PuzzleSizeSelectorComponent } from './puzzle-size-selector/puzzle-size-selector.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PuzzleSizeSelectorComponent
+    PuzzleSizeSelectorComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Capstone/puzzle-app/src/app/puzzle-size-selector/puzzle-size-selector.component.ts
+++ Capstone/puzzle-app/src/app/puzzle-size-selector/puzzle-size-selector.component.ts
@@ -3,17 +3,20 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-puzzle-size-selector',
   templateUrl: './puzzle-size-selector.component.html',
-  styleUrls: ['./puzzle-size-selector.component.css']
+  styleUrls: ['./puzzle-size-selector.component.scss']
 })
 export class PuzzleSizeSelectorComponent implements OnInit {
-
-  @Input() puzzleType;
-  constructor() { }
-
-  ngOnInit() {
-    console.log(puzzleSizes[this.puzzleType]);
+  @Input() puzzleType: string;
+  puzzleSizes: {"Hitori": Array<string>, "Heyawake": Array<string>};
+  constructor() {
+    this.puzzleSizes = puzzleSizes;
   }
+
+  ngOnInit() { }
 
 }
 
-export const puzzleSizes = {"Hitori": ["5x5", "6x6", "7x7", "8x8", "9x9"], "Heyawake": ["9x9", "10x10", "11x11", "15x15"]};
+const puzzleSizes = {
+  "Hitori": ["5x5", "6x6", "7x7", "8x8", "9x9"],
+  "Heyawake": ["9x9", "10x10", "11x11", "15x15"]
+};
