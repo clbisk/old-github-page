b59501cb3a2691004306b23f17de048d333e018c
Cecilia Bisk <clbisk@gmail.com>
2020-01-25 11:19:11 -0500

    added route param reading to puzzle view

--- Capstone/puzzle-app/src/app/puzzle-view/puzzle-view.component.ts
+++ Capstone/puzzle-app/src/app/puzzle-view/puzzle-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-puzzle-view',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puzzle-view.component.css']
 })
 export class PuzzleViewComponent implements OnInit {
+  puzzleType: string;
+  id: number;
 
-  constructor() { }
+  private routeSubscription;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe(params => {
+      this.puzzleType = params['puzzle-type'];
+      this.id = +params['id']
+    });
   }
 
+  ngOnDestroy() {
+    //clean up clean up
+    this.routeSubscription.unsubscribe();
+  }
 }
