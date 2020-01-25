4ab9ac0c7143ea29f819e324c35adf3659517bbd
Cecilia Bisk <clbisk@gmail.com>
2020-01-25 17:40:40 -0500

    transitioning to scss default

--- Capstone/puzzle-app/src/app/hitori/hitori.component.ts
+++ Capstone/puzzle-app/src/app/hitori/hitori.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hitori',
   templateUrl: './hitori.component.html',
-  styleUrls: ['./hitori.component.css']
+  styleUrls: ['./hitori.component.scss']
 })
 export class HitoriComponent implements OnInit {
 
