1194c4ecc8f0a4c685e06bf1402ef57cbdbfddea
Cecilia Bisk <clbisk@gmail.com>
2020-01-25 22:42:27 -0500

    added grid to heyawake display

--- Capstone/puzzle-app/src/app/heyawake/heyawake.component.ts
+++ Capstone/puzzle-app/src/app/heyawake/heyawake.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heyawake.component.scss']
 })
 export class HeyawakeComponent implements OnInit {
+  size: number;
+  puzzleData: Array<Array<boolean | null>> = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.size = 5;
+    for (let i = 0; i < this.size; i++) {
+      this.puzzleData.push(new Array<boolean | null>(this.size));
+    }
   }
-
 }
