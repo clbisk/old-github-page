027982e549e1da6e72243a9d1dcb50413b7147e1
Cecilia Bisk <clbisk@gmail.com>
2020-01-26 12:09:23 -0500

    added data structure for heyawake puzzles

--- Capstone/puzzle-app/src/app/heyawake/heyawake.component.ts
+++ Capstone/puzzle-app/src/app/heyawake/heyawake.component.ts
@@ -7,14 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeyawakeComponent implements OnInit {
   size: number;
-  puzzleData: Array<Array<boolean | null>> = [];
+  roomBoundaries: Array<Room> = [];
+  userSolution: Array<Array<boolean | null>> = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.size = 5;
+    this.size = 6;
     for (let i = 0; i < this.size; i++) {
-      this.puzzleData.push(new Array<boolean | null>(this.size));
+      this.userSolution.push(new Array<boolean | null>(this.size));
     }
+
+    this.roomBoundaries = [
+      new Room([0,0], [2,1], 2),
+      new Room([3,0], [5,1])
+    ]
+  }
+}
+
+export class Room {
+  clue: number | null; //number of painted cells in the puzzle
+  topLeft: [number, number]; //first cell in the room at the top left corner
+  bottomRight: [number, number]; //last cell in the room at the bottom right corner
+
+  constructor(topLeft: [number, number], bottomRight: [number, number], clue?: number) {
+    this.topLeft = topLeft;
+    this.bottomRight = bottomRight;
+    this.clue = clue? clue: null;
   }
 }
